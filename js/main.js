f7a7377938207816d6fc5893f30c21662081b273
Adrian Carabetta <adrian_carabetta@hotmail.com>
2018-06-06 20:09:40 +0800

    removed some tstuff

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slide-show').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 300,
+    dots: true,
+    autoplay: true
+  });
+});
